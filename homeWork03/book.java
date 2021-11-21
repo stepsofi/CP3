9ec5e6a6c0cc6207cb3a0a6342c0f29f49a861d5
stepsofi <79860096+stepsofi@users.noreply.github.com>
2021-11-21 18:27:54 +0100

    homeWork3

---
+++ homeWork03/book.java
@@ -0,0 +1,21 @@
+public class book
+{
+    private String title;
+    private String author;
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getAuthor(){
+        return author;
+    }
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public void setAuthor(String author){
+        this.author = author;
+    }
+}
